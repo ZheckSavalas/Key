96dc5e0c4792e293b9e9cd29cce3f7b9fe7ecc88
Zheck Savalas <57382953+ZheckSavalas@users.noreply.github.com>
2019-11-11 10:05:11 +0700

    Add files via upload

---
+++ Key.py
@@ -0,0 +1,40 @@
+import os
+from time import sleep
+
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+os.system('clear')
+print(a+'\t  Shorcut for help you')
+print(b+'\t    Zheck Savalas')
+print('\t http://bit.ly/S4vLs')
+print(a+'+'*40)
+print('\nProses..')
+sleep(1)
+print(b+'\n[!] making termux properties directory..')
+sleep(1)
+try:
+      os.mkdir('/data/data/com.termux/files/home/.termux')
+except:
+      pass
+print(a+'[!]Success !')
+sleep(1)
+print(b+'\n[!] Making setup file..')
+sleep(1)
+
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
+kontol.write(key)
+kontol.close()
+sleep(1)
+print(a+'[!] Success !')
+sleep(1)
+print(b+'\n[!] Setting up..')
+sleep(2)
+os.system('termux-reload-settings')
+print(a+'[!] Successfully !! ^^'+c+'\n\nhubungi ZheckSavalas untuk requests\natau pertanyaan, atau hubungi http://bit.ly/S4vLs/nThanks :*\n\n')
+
+
+# ini cuma shortcut buat bantu para nub
+# Zheck Savalas
